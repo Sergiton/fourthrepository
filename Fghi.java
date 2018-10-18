7ddbf886380d8428bf8b89b3ebae26d46ad13f04
babybluedino <sergitondav@gmail.com>
2018-10-18 09:45:15 +0530

    Hai

---
+++ Fghi.java
@@ -0,0 +1,7 @@
+public class Fghi {
+public void display()
+{
+System.out.println("Main method in Fghi");
+
+}
+}
--- Rstu.java
+++ Rstu.java
@@ -2,6 +2,6 @@ public class Rstu {
 public static void main (String ags[])
 {
 System.out.println("Main method in Rstu");
-
+new Fghi().display();
 }
 }
